74b7f759e709f1639464df775f3277be9f4d3367
cruzer3008 <31630057+cruzer3008@users.noreply.github.com>
2018-07-20 13:47:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,12 +58,15 @@ preloadImage("https://raw.githubusercontent.com/QwertyWorks/qwertyworks.github.i
   }
 	if(transcript.includes("red"))
 	   alert("Red"); 
-	if(transcript.includes("blue"))
-		document.getElementById("bulbPhoto").src = url("https://raw.githubusercontent.com/QwertyWorks/qwertyworks.github.io/master/Interns/js/images/rightback.png");
+	if(transcript.includes("blue")){
+		var blueBulb = document.getElementById("bulbPhoto");
+		blueBulb.src = url("https://raw.githubusercontent.com/QwertyWorks/qwertyworks.github.io/master/Interns/js/images/rightback.png");
+	}
 	if(transcript.includes("green"))
 	   alert("Green");
-	if(transcript.includes("yellow"))
-		document.getElementById("bulbPhoto").src = url("https://raw.githubusercontent.com/QwertyWorks/qwertyworks.github.io/master/Interns/js/images/leftback.png");
+	if(transcript.includes("yellow")){
+		var yellowBulb = document.getElementById("bulbPhoto");
+		yellowBulb.src = url("https://raw.githubusercontent.com/QwertyWorks/qwertyworks.github.io/master/Interns/js/images/leftback.png");
 	
 	if(transcript.includes("turn on"))
 	   alert("Switched On");
